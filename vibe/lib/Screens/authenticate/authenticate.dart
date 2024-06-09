3bd6ef006dd4b440ab10594340b5893d46cfd377
Raj-Surase <rajsurase3@gmail.com>
2024-06-09 19:59:12 +0530

    Added google button to authentication screen

--- vibe/lib/Screens/authenticate/authenticate.dart
+++ vibe/lib/Screens/authenticate/authenticate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vibe/Constants/colors.dart';
+import 'package:vibe/Constants/typography.dart';
+import 'package:vibe/Constants/values.dart';
 import 'package:vibe/Screens/authenticate/login.dart';
 
 class Authenticate extends StatefulWidget {
@@ -12,10 +15,44 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(
+            ValuesConstants.paddingLR,
+            ValuesConstants.paddingTB,
+            ValuesConstants.paddingLR,
+            ValuesConstants.paddingTB),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.g_mobiledata_rounded,
+                    color: AppColor.textHighEm,
+                    size: 24,
+                  ),
+                  Text(
+                    "Google",
+                    style: AppTypography.textStyle14Bold,
+                  )
+                ],
+              ),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(AppColor.primaryButton),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(ValuesConstants.radiusMedium),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
