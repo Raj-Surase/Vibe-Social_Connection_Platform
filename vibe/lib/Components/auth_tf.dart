b0c0e3d59fd3eeb77ab93432cebe1d638af64f34
Raj-Surase <rajsurase3@gmail.com>
2024-06-20 11:24:23 +0530

    database integration successfully completed. profile initialization also completed.

--- vibe/lib/Components/auth_tf.dart
+++ vibe/lib/Components/auth_tf.dart
@@ -65,7 +65,6 @@ class _AuthTextFieldState extends State<AuthTextField> {
     return TextFormField(
       controller: widget.controller,
       obscureText: widget.isSecure,
-      autofocus: true,
       textInputAction: TextInputAction.next,
       cursorRadius: const Radius.circular(10),
       cursorColor: AppColor.componentActive,
